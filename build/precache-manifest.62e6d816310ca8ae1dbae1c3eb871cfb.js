b88ef1d1e51b7074d2ef996b5055a447a9af73fb
Giovane Marcelo <giovane0845@gmail.com>
2021-06-03 23:46:53 -0300

    build

---
+++ build/precache-manifest.62e6d816310ca8ae1dbae1c3eb871cfb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56bc0f94f98239338406ef8566d20bed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2354df97c294e6902ad7",
+    "url": "/static/css/2.b0704b7a.chunk.css"
+  },
+  {
+    "revision": "1b08f2eb19af3b551ad7",
+    "url": "/static/css/main.28044fc0.chunk.css"
+  },
+  {
+    "revision": "2354df97c294e6902ad7",
+    "url": "/static/js/2.d96d6e41.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.d96d6e41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b08f2eb19af3b551ad7",
+    "url": "/static/js/main.021ff91d.chunk.js"
+  },
+  {
+    "revision": "7840a4360155d662a69e",
+    "url": "/static/js/runtime-main.49e9a94d.js"
+  }
+]);
